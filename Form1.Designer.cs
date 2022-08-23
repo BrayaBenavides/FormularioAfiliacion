5c2412e0b60ad5ba3d93bca47d9ca0d3ebda1c3a
Brayan Benavides <stiven.1001089996@gmail.com>
2022-08-22 20:41:38 -0500

    primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace FormularioExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnImportar = new System.Windows.Forms.Button();
+            this.LblFile = new System.Windows.Forms.Label();
+            this.DataDetalles = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataDetalles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnImportar
+            // 
+            this.BtnImportar.Location = new System.Drawing.Point(35, 28);
+            this.BtnImportar.Name = "BtnImportar";
+            this.BtnImportar.Size = new System.Drawing.Size(75, 23);
+            this.BtnImportar.TabIndex = 0;
+            this.BtnImportar.Text = "Importar";
+            this.BtnImportar.UseVisualStyleBackColor = true;
+            this.BtnImportar.Click += new System.EventHandler(this.BtnImportar_Click);
+            // 
+            // LblFile
+            // 
+            this.LblFile.AutoSize = true;
+            this.LblFile.Location = new System.Drawing.Point(134, 33);
+            this.LblFile.Name = "LblFile";
+            this.LblFile.Size = new System.Drawing.Size(35, 13);
+            this.LblFile.TabIndex = 1;
+            this.LblFile.Text = "label1";
+            // 
+            // DataDetalles
+            // 
+            this.DataDetalles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataDetalles.Location = new System.Drawing.Point(55, 91);
+            this.DataDetalles.Name = "DataDetalles";
+            this.DataDetalles.Size = new System.Drawing.Size(684, 232);
+            this.DataDetalles.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DataDetalles);
+            this.Controls.Add(this.LblFile);
+            this.Controls.Add(this.BtnImportar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataDetalles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnImportar;
+        private System.Windows.Forms.Label LblFile;
+        private System.Windows.Forms.DataGridView DataDetalles;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnImportar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog = new OpenFileDialog
+            {
+                Filter = "Excel | *.csv; *.xlsx;",
+                Title = "Seleccionar archivo"
+
+            };
+
+            if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                DataDetalles.DataSource = ImportarDatos(OpenFileDialog.FileName);
+            }
+
+            LblFile.Text = OpenFileDialog.FileName;
+        }
+    
+
+        DataView ImportarDatos(string nombrearchivo)
+        {
+            string conexion = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}; Extended properties = 'Excel 12.0;'", nombrearchivo);
+
+            OleDbConnection conector = new OleDbConnection(conexion);
+
+            conector.Open();
+
+            OleDbCommand consulta = new OleDbCommand("select * from [Hoja1$]", conector);
+
+            OleDbDataAdapter adaptador = new OleDbDataAdapter
+            {
+                SelectCommand = consulta
+            };
+
+            DataSet ds = new DataSet();
+            adaptador.Fill(ds);
+
+            conector.Close();
+
+            return ds.Tables[0].DefaultView;
+
+        }
+        
+
+    }
+}
